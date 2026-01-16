e40bc754b3da6c5e455142e0c8d9a58f1b641d3d
张哲凯2023051604081 <3364998180@qq.com>
2026-01-16 15:16:32 +0800

    新增消息列表模型 MessageListModel，支持消息状态更新

--- src/models/message_list_model.cpp
+++ src/models/message_list_model.cpp
@@ -1,27 +1,103 @@
 #include "models/message_list_model.h"
 
-namespace ShirohaChat
-{
+#include <QUuid>
 
-MessageListModel::MessageListModel(QObject* parent)
-    : QAbstractListModel(parent)
-{
-}
+namespace ShirohaChat {
 
-int MessageListModel::rowCount(const QModelIndex& parent) const
-{
-    Q_UNUSED(parent);
+    MessageListModel::MessageListModel(
+        QObject* parent)
+        : QAbstractListModel(parent)
+    {}
 
-}
+    int MessageListModel::rowCount(
+        const QModelIndex& parent) const
+    {
+        Q_UNUSED(parent)
+        return m_messages.count();
+    }
 
-QVariant MessageListModel::data(const QModelIndex& index, int role) const
-{
-    // TODO: Implement
-}
+    QVariant MessageListModel::data(
+        const QModelIndex& index, int role) const
+    {
+        if (!index.isValid() || index.row() >= m_messages.count())
+            return QVariant();
 
-QHash<int, QByteArray> MessageListModel::roleNames() const
-{
-   // TODO: Implement
-}
+        const auto& message = m_messages.at(index.row());
+
+        switch (role) {
+        case MessageIdRole:
+            return message.messageId;
+        case SenderIdRole:
+            return message.senderId;
+        case ContentRole:
+            return message.content;
+        case IsMineRole:
+            return message.isMine;
+        case StatusRole:
+            return message.status;
+        case TimestampRole:
+            return message.timestamp;
+        default:
+            return QVariant();
+        }
+    }
+
+    QHash<int, QByteArray> MessageListModel::roleNames() const
+    {
+        QHash<int, QByteArray> roles;
+        roles[MessageIdRole] = "messageId";
+        roles[SenderIdRole] = "senderId";
+        roles[ContentRole] = "content";
+        roles[IsMineRole] = "isMine";
+        roles[StatusRole] = "status";
+        roles[TimestampRole] = "timestamp";
+        return roles;
+    }
+
+    void MessageListModel::updateStatus(
+        const QString& msgId, int newStatus)
+    {
+        for (int i = 0; i < m_messages.count(); ++i) {
+            if (m_messages[i].messageId == msgId) {
+                m_messages[i].status = newStatus;
+                const auto idx = index(i);
+                emit dataChanged(idx, idx, {StatusRole});
+                break;
+            }
+        }
+    }
+
+    void MessageListModel::updateStatus(
+        const QString& msgId, MessageStatus newStatus)
+    {
+        updateStatus(msgId, static_cast<int>(newStatus));
+    }
+
+    QString MessageListModel::appendMessage(
+        const QString& senderId, const QString& content, bool isMine, const QString& timestamp)
+    {
+        const auto msgId = QUuid::createUuid().toString(QUuid::WithoutBraces);
+
+        const int row = m_messages.size();
+        beginInsertRows(QModelIndex(), row, row);
+        m_messages.append({msgId, senderId, content, isMine, isMine ? 0 : 1, timestamp});
+        endInsertRows();
+
+        return msgId;
+    }
+
+    void MessageListModel::appendMessage(
+        const Message& msg, bool isMine)
+    {
+        const int row = m_messages.size();
+        beginInsertRows(QModelIndex(), row, row);
+        m_messages.append({msg.msgId(),
+                           msg.senderId(),
+                           msg.content(),
+                           isMine,
+                           static_cast<int>(msg.status()),
+                           msg.timestamp().toString(Qt::ISODate)});
+        endInsertRows();
+    }
 
 } // namespace ShirohaChat
--- src/models/message_list_model.h
+++ src/models/message_list_model.h
@@ -1,19 +1,60 @@
 #pragma once
 
 #include <QAbstractListModel>
+#include <QList>
+#include <QString>
 
-namespace ShirohaChat
-{
+#include "domain/message.h"
 
-class MessageListModel : public QAbstractListModel
-{
-    Q_OBJECT
+namespace ShirohaChat {
 
-  public:
-    explicit MessageListModel(QObject* parent = nullptr);
-    int rowCount(const QModelIndex& parent = QModelIndex()) const override;
-    QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const override;
-    QHash<int, QByteArray> roleNames() const override;
-};
+    /**
+ * @brief 消息列表模型
+ */
+    class MessageListModel : public QAbstractListModel
+    {
+        Q_OBJECT
+
+    public:
+        enum MessageRoles {
+            MessageIdRole = Qt::UserRole + 1,
+            SenderIdRole,
+            ContentRole,
+            IsMineRole,
+            StatusRole,
+            TimestampRole
+        };
+        Q_ENUM(
+            MessageRoles)
+
+        explicit MessageListModel(QObject* parent = nullptr);
+
+        int rowCount(const QModelIndex& parent = QModelIndex()) const override;
+        QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const override;
+        QHash<int, QByteArray> roleNames() const override;
+
+        Q_INVOKABLE void updateStatus(const QString& msgId, int newStatus);
+        void updateStatus(const QString& msgId, MessageStatus newStatus);
+
+        Q_INVOKABLE QString appendMessage(const QString& senderId,
+                                          const QString& content,
+                                          bool isMine,
+                                          const QString& timestamp);
+
+        void appendMessage(const Message& msg, bool isMine);
+
+    private:
+        struct MessageData
+        {
+            QString messageId;
+            QString senderId;
+            QString content;
+            bool isMine;
+            int status;
+            QString timestamp;
+        };
+
+        QList<MessageData> m_messages;
+    };
 
 } // namespace ShirohaChat
